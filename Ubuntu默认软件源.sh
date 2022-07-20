155227c7f0c41f736af583e5038d5495a86bdb8b
imetthegirl <66664991+imetthegirl@users.noreply.github.com>
2022-07-20 14:34:25 +0800

    Update Ubuntu默认软件源.sh

--- Ubuntu默认软件源.sh
+++ Ubuntu默认软件源.sh
@@ -1,16 +1,23 @@
 #Ubuntu 16.04
 #/etc/apt/sources.list
 #阿里源
-deb http://mirrors.aliyun.com/ubuntu/ quantal main restricted universe multiverse
-deb http://mirrors.aliyun.com/ubuntu/ quantal-security main restricted universe multiverse
-deb http://mirrors.aliyun.com/ubuntu/ quantal-updates main restricted universe multiverse
-deb http://mirrors.aliyun.com/ubuntu/ quantal-proposed main restricted universe multiverse
-deb http://mirrors.aliyun.com/ubuntu/ quantal-backports main restricted universe multiverse
-deb-src http://mirrors.aliyun.com/ubuntu/ quantal main restricted universe multiverse
-deb-src http://mirrors.aliyun.com/ubuntu/ quantal-security main restricted universe multiverse
-deb-src http://mirrors.aliyun.com/ubuntu/ quantal-updates main restricted universe multiverse
-deb-src http://mirrors.aliyun.com/ubuntu/ quantal-proposed main restricted universe multiverse
-deb-src http://mirrors.aliyun.com/ubuntu/ quantal-backports main restricted universe multiverse
+deb-src http://archive.ubuntu.com/ubuntu xenial main restricted #Added by software-properties
+deb http://mirrors.aliyun.com/ubuntu/ xenial main restricted
+deb-src http://mirrors.aliyun.com/ubuntu/ xenial main restricted multiverse universe #Added by software-properties
+deb http://mirrors.aliyun.com/ubuntu/ xenial-updates main restricted
+deb-src http://mirrors.aliyun.com/ubuntu/ xenial-updates main restricted multiverse universe #Added by software-properties
+deb http://mirrors.aliyun.com/ubuntu/ xenial universe
+deb http://mirrors.aliyun.com/ubuntu/ xenial-updates universe
+deb http://mirrors.aliyun.com/ubuntu/ xenial multiverse
+deb http://mirrors.aliyun.com/ubuntu/ xenial-updates multiverse
+deb http://mirrors.aliyun.com/ubuntu/ xenial-backports main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ xenial-backports main restricted universe multiverse #Added by software-properties
+deb http://archive.canonical.com/ubuntu xenial partner
+deb-src http://archive.canonical.com/ubuntu xenial partner
+deb http://mirrors.aliyun.com/ubuntu/ xenial-security main restricted
+deb-src http://mirrors.aliyun.com/ubuntu/ xenial-security main restricted multiverse universe #Added by software-properties
+deb http://mirrors.aliyun.com/ubuntu/ xenial-security universe
+deb http://mirrors.aliyun.com/ubuntu/ xenial-security multiverse
 
 #清华源
 deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ xenial main restricted
